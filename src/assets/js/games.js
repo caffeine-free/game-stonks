fb7700bf4f090d0488e5210d650b96f21fa4805e
c-bele <cibele.ferreira@aluno.ufop.edu.br>
2021-04-04 18:30:50 -0300

    feat: registers users and games

---
+++ src/assets/js/games.js
@@ -0,0 +1,11 @@
+const data = []
+
+const post = () => {
+    const name = document.getElementById("name")?.value;
+    const price = document.getElementById("price")?.value;
+    const description = document.getElementById("description")?.value;
+    const id = Math.random().toString(32).substr(2, 9)
+    data.push({ id, name, price, description })
+    console.log(JSON.stringify(data));
+    alert('Game successfully registered!');
+}
